810c890344a2388a3ee24b99b9074a15e6c0ca85
avasekova <avaseko@gmail.com>
2016-03-16 10:09:42 +0100

    margins, axes

--- src/main/resources/me.deadcode.adka.d3git/main.js
+++ src/main/resources/me.deadcode.adka.d3git/main.js
@@ -1,39 +1,57 @@
 "use strict";
 
 //initialize as much as we can before the data is available
-var width = 420,
-    barHeight = 20;
+var margin = {top: 20, right: 30, bottom: 30, left: 40};
+var chartWidth = 800 - margin.left - margin.right;
+var chartHeight = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, chartWidth], .1);
+
+var y = d3.scale.linear()
+    .range([chartHeight, 0]); //the origin of SVG’s coordinate system is in the top-left corner
+
+var xAxis = d3.svg.axis()
+    .scale(x) //bind the axis to a scale
+    .orient("bottom");
+//the resulting xAxis object can be used to render multiple axes by repeated application using selection.call
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", chartWidth + margin.left + margin.right)
+    .attr("height", chartHeight + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //any elements subsequently added to chart will inherit the margins
 
 //then finish the rest in the callback once the data is downloaded
 d3.csv("data.csv", type, function(error, data) {
     //asynchronous: code here runs after the download finishes
 
-    console.log(data[0]);
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]); //the second arg to max is an 'accessor function'
 
-    x.domain([0, d3.max(data, function(d) { return d.value; })]); //the second arg to max is an 'accessor function'
+    chart.append("g")
+        .attr("class", "x axis") //actually, two classes: 'x' and 'axis'
+        .attr("transform", "translate(0," + chartHeight + ")") //the axis elements are positioned relative to a local origin
+        .call(xAxis);
 
-    chart.attr("height", barHeight * data.length);
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
-    var bar = chart.selectAll("g") //'grouping element' in SVG
+
+    chart.selectAll(".bar")
         .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
-        .attr("height", barHeight - 1);
-
-    bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.value; });
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.name); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return chartHeight - y(d.value); })
+        .attr("width", x.rangeBand());
 });
 //code here runs while the file is downloading
 

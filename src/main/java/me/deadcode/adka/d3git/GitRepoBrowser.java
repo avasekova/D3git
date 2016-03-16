8807d9cab0f412e591e60fb219ad32e2f55c8530
avasekova <avaseko@gmail.com>
2016-03-16 17:17:59 +0100

    JGit integration; get commits by branch for a given local repository

---
+++ src/main/java/me/deadcode/adka/d3git/GitRepoBrowser.java
@@ -0,0 +1,10 @@
+package me.deadcode.adka.d3git;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public interface GitRepoBrowser {
+
+    Map<String, List<CommitInfo>> getAllCommits(File gitDirectory);
+}
---
+++ src/main/java/me/deadcode/adka/d3git/LocalGitRepoBrowser.java
@@ -0,0 +1,50 @@
+package me.deadcode.adka.d3git;
+
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LocalGitRepoBrowser implements GitRepoBrowser {
+
+    @Override
+    public Map<String, List<CommitInfo>> getAllCommits(File gitDirectory) {
+        Map<String, List<CommitInfo>> commitsByBranch = new HashMap<>();
+
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+
+        try (Repository repository = builder.setGitDir(gitDirectory).build();
+             Git git = new Git(repository)) {
+
+            for (Ref ref : git.branchList().call()) {
+                List<CommitInfo> commits = new ArrayList<>();
+
+                git.log()
+                        .add(ref.getObjectId()) //start graph traversal from this branch
+                        .call()
+                        .forEach(i -> commits.add(new CommitInfo(Instant.ofEpochSecond(i.getCommitTime()),
+                                i.getAuthorIdent().getName(),
+                                i.getAuthorIdent().getEmailAddress(),
+                                i.getName(),
+                                i.getFullMessage().trim())));
+
+                String[] branchNameParts = ref.getName().split("/");
+                commitsByBranch.put(branchNameParts[branchNameParts.length - 1], commits);
+            }
+        } catch (GitAPIException | IOException e) {
+            //TODO log
+        }
+
+        return commitsByBranch;
+    }
+}
